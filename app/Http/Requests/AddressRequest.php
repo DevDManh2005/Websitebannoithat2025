6ceb1f5f7ddfc1c64a029532dbf85feb0a935657
Lê Đức Mạnh <manhldpd10554@gmail.com>
2025-08-24 19:57:07 +0700

    1
    1

--- app/Http/Requests/AddressRequest.php
+++ app/Http/Requests/AddressRequest.php
@@ -11,17 +11,24 @@ class AddressRequest extends FormRequest
         return true;
     }
 
-    public function rules(): array
-    {
-        return [
-            'receiver_name' => ['required', 'string', 'max:255'],
-            'phone'         => ['required', 'regex:/^(0[1-9][0-9]{8,9}|(\+84)[1-9][0-9]{7,9})$/'],
-            'city'          => ['required', 'string', 'max:255'],
-            'district'      => ['required', 'string', 'max:255'],
-            'ward'          => ['required', 'string', 'max:255'],
-            'address'       => ['required', 'string', 'max:255'],
-        ];
-    }
+  public function rules(): array
+{
+    return [
+        'receiver_name' => ['required', 'string', 'max:255'],
+        'phone'         => ['required', 'regex:/^(0[1-9][0-9]{8,9}|(\+84)[1-9][0-9]{7,9})$/'],
+        'city'          => ['required', 'string', 'max:255'],
+        'district'      => ['required', 'string', 'max:255'],
+        'ward'          => ['required', 'string', 'max:255'],
+        'address'       => ['required', 'string', 'max:255'],
+        'note'          => ['nullable', 'string'],
+        'shipping_fee'  => ['required', 'numeric', 'min:0'],
+        'payment_method'=> ['required', 'in:cod,vnpay'],
+        'province_id'   => ['required', 'integer'],
+        'district_id'   => ['required', 'integer'],
+        'ward_code'     => ['required', 'string'],
+    ];
+}
+
 
     public function messages(): array
     {
